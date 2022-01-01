6bc1fc9c6791c1dc7191440a7c0e54571331a492
timothyliutl <18tfl1@queensu.ca>
2022-01-01 05:57:20 -0500

    edited data so there wasn't leakage

--- movingAverage.py
+++ movingAverage.py
@@ -19,6 +19,7 @@ data['date'] = data['date'].apply(lambda x: datetime.strptime(x, '%d-%m-%y'))
 data['playerHS'] = data['playerHS'].apply(lambda x: float(str(x).replace('%', '')))
 sorted_data = data.sort_values(by=['playerName','date'])
 used_cols = ['playerName','playerKills', 'playerDeaths', 'playerAssists', 'playerACS', 'playerADR', 'playerHS', 'playerFirstBlood', 'playerFirstDeath']
+sorted_data[used_cols] = sorted_data[used_cols].shift(1)
 sorted_data[used_cols] = sorted_data.groupby('playerName')[used_cols].rolling(10).mean().reset_index().set_index('level_1')[used_cols]
 sorted_data[sorted_data['playerName']=='zombs']
-sorted_data.to_csv('movingAverage.csv')
+sorted_data.to_csv('movingAverage2.csv')
